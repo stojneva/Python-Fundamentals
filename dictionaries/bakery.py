2e082bb0ce7812fabf11ddc0ef418e63bae165fe
oem <yanastoyneva33@gmail.com>
2023-05-14 22:56:50 +0300

    new

---
+++ dictionaries/bakery.py
@@ -0,0 +1,11 @@
+elements = input().split(" ")
+bakery = {}
+
+for index in range(0, len(elements), 2):
+    key = elements[index]
+    value = elements[index + 1]
+    bakery[key] = int(value)
+print(bakery)
+
+
+
---
+++ dictionaries/dictionary_test.py
@@ -0,0 +1,10 @@
+yana_dictionary = {"name": "Yana", "age": "26", "town": "Sofia"}
+# print(yana_dictionary["name"])
+# print(yana_dictionary["age"])
+# print(yana_dictionary["town"])
+#
+# if "name" in yana_dictionary.keys():
+#     print(yana_dictionary["name"])
+
+for x in yana_dictionary.values():
+    print(x)
