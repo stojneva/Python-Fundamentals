ea2b0ebfa746baf53151b3d0ee373c004123dd3c
oem <yanastoyneva33@gmail.com>
2023-06-07 00:59:16 +0300

    add

--- dictionaries/stock.py
+++ dictionaries/stock.py
@@ -1,13 +1,1 @@
-products = input().split()
-search_products = input().split()
 
-product_dict = {}
-
-for x in range(0, len(products), 2):
-    product_dict[products[x]] = products[x + 1]
-
-for x in search_products:
-    if x in product_dict:
-        print(f"We have {product_dict[x]} of {x} left")
-    else:
-        print(f"Sorry, we don't have {x}")
